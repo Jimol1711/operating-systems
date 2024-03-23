6ed44ab00e5c0bb8635b3874b1422a065738f7c1
Jimol1711 <juan.molina.l@ug.uchile.cl>
2024-03-23 18:14:00 -0300

    Creation of args struct and void function

--- Tareas/T1/maleta.c
+++ Tareas/T1/maleta.c
@@ -13,8 +13,43 @@
 
 // Defina aca las estructuras y funciones adicionales que necesite
 // ...
+# if 0
+typedef struct{
+    double *w;
+    double *v;
+    int *z;
+    int n;
+    double maxW;
+    double k;
+} Args;
+
+void *thread(void *p) {
+    Args *pargs = (Args *) p;
+    double w[] = pargs->w;
+    llenarMaletaSec( );
+    return NULL;
+} 
+# endif
+
+// Estructura para los argumentos de la función
+typedef struct {
+    double *w;
+    double *v;
+    int *z;
+    int n;
+    double maxW;
+    int k;
+} ThreadArgs;
+
+// Declaración de funcion para phtread_create
+void *thread(void *args) {
+    ThreadArgs *pargs = (ThreadArgs *)args;
+    llenarMaletaSec(pargs->w, pargs->v, pargs->z, pargs->n, pargs->maxW, pargs->k / 8);
+    return NULL;
+};
+
 
 double llenarMaletaPar(double w[], double v[], int z[], int n,
                        double maxW, int k) {
-  
+
 }
