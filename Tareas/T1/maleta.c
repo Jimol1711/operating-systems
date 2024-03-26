98cf873852f98f8571728a62e1c92cc1198d6070
Jimol1711 <juan.molina.l@ug.uchile.cl>
2024-03-26 12:59:13 -0300

    Struggling with logic for args structure

--- Tareas/T1/maleta.c
+++ Tareas/T1/maleta.c
@@ -29,42 +29,39 @@ void *thread(void *args) {
     return NULL;
 }
 
-
+// Función que calcula en paralelo en 8 threads
 double llenarMaletaPar(double w[], double v[], int z[], int n,
                        double maxW, int k) {
     pthread_t pids[8];
-    Args *args_array = malloc(8 * sizeof(Args));
-    
-    for (int i = 0; i < 8; i++) {
-        
-        args_array[i].w = malloc(n * sizeof(double));
-        args_array[i].v = malloc(n * sizeof(double));
-        args_array[i].z = malloc(n * sizeof(int));
+    Args args_array[8];
 
+    for (int i = 0; i < 8; i++) {
+
+        // Arreglo z para cada thread
+        int *z_thread = malloc(n * sizeof(int));
+
+        // Asignación de elementos de w al arreglo args_array (Esto ocurre en cada thread independientemente)
         for (int j = 0; j < n; j++) {
             args_array[i].w[j] = w[j];
             args_array[i].v[j] = v[j];
-            args_array[i].z[j] = z[j];
         }
 
-        args_array[i].n = n;
+        // Asignación de n, maxW y k
+        args_array[i].n = maxW;
         args_array[i].maxW = maxW;
         args_array[i].k = k;
-
         
         pthread_create(&pids[i], NULL, thread, &args_array[i]);
+
+        z_thread = args_array[i].z;
+
+        free(z_thread);
     }
-    
+
+    // Entierro de los threads
     for (int i = 0; i < 8; i++) {
         pthread_join(pids[i], NULL);
     }
 
-    for (int i = 0; i < 8; i++) {
-        free(args_array[i].w);
-        free(args_array[i].v);
-        free(args_array[i].z);
-    }
-    free(args_array);
-
-    return 0; // Change the return type if needed
+    return 0;
 }
