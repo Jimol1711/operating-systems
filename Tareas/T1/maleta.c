80142045ce5aea7e52c80c7a6567fb8759676db8
Jimol1711 <juan.molina.l@ug.uchile.cl>
2024-03-26 23:05:28 -0300

    Debugger for vscode added

--- Tareas/T1/maleta.c
+++ Tareas/T1/maleta.c
@@ -1,5 +1,3 @@
-// Plantilla para maleta.c
-
 #define _XOPEN_SOURCE 500
 
 #include <stdio.h>
@@ -25,7 +23,7 @@ typedef struct {
 // Declaración de funcion para phtread_create
 void *thread(void *args) {
     Args *pargs = (Args *)args;
-    llenarMaletaSec(pargs->w, pargs->v, pargs->z, pargs->n, pargs->maxW, pargs->k / 8);
+    llenarMaletaSec(pargs->w, pargs->v, pargs->z, pargs->n, pargs->maxW, (pargs->k) / 8);
     return NULL;
 }
 
@@ -34,33 +32,55 @@ double llenarMaletaPar(double w[], double v[], int z[], int n,
                        double maxW, int k) {
     pthread_t pids[8];
     Args args_array[8];
+    double bestSum = -1;
 
     for (int i = 0; i < 8; i++) {
 
         // Arreglo z para cada thread
         int *z_thread = malloc(n * sizeof(int));
 
-        // Asignación de elementos de w al arreglo args_array (Esto ocurre en cada thread independientemente)
+        // Alocando memoria para w y v
+        args_array[i].w = malloc(n * sizeof(double));
+        args_array[i].v = malloc(n * sizeof(double));
+
+        // Asignación de elementos de w y v al arreglo args_array (Esto ocurre en cada thread independientemente)
         for (int j = 0; j < n; j++) {
             args_array[i].w[j] = w[j];
             args_array[i].v[j] = v[j];
         }
 
         // Asignación de n, maxW y k
-        args_array[i].n = maxW;
+        args_array[i].n = n;
         args_array[i].maxW = maxW;
         args_array[i].k = k;
+
+        // Asignación del arreglo z
+        args_array[i].z = z_thread;
         
         pthread_create(&pids[i], NULL, thread, &args_array[i]);
 
-        z_thread = args_array[i].z;
-
-        free(z_thread);
+        // Después de hacer el calculo asigna el mejor array z_thread a z
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            if (z_thread[i] != 0) {
+                sum += v[i];
+            }
+            if (sum > bestSum) {
+                bestSum = sum;
+                z = z_thread;
+            }
+        }
     }
 
     // Entierro de los threads
     for (int i = 0; i < 8; i++) {
         pthread_join(pids[i], NULL);
+    }
+
+    // Free memory allocated for w and v in args_array
+    for (int i = 0; i < 8; i++) {
+        free(args_array[i].w);
+        free(args_array[i].v);
     }
 
     return 0;
