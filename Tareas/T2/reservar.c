e822f01af6b89d6b7377efc8c9208b25ec71f590
Jimol1711 <juan.molina.l@ug.uchile.cl>
2024-04-03 16:10:19 -0300

    AUx4 added

--- Tareas/T2/reservar.c
+++ Tareas/T2/reservar.c
@@ -8,19 +8,22 @@
 #include "reservar.h"
 
 // Defina aca las variables globales y funciones auxiliares que necesite
+pthread_cond_t c = PTHREAD_COND_INITIALIZER;
 
 void initReservar() {
-  
+    int *estacionamientos[10];
 }
 
 void cleanReservar() {
   
 }
 
-int reservar(int k) {
-
+int reservar(int k) { // k es el número de estacionamientos requerido
+    // Si hay k estacionamientos contiguos los reserva y retorna id del primer estacionamiento en la serie otorgada
+    // En caso contrario, espera hasta que hayan k estacionamientos contiguos disponibles.
 }
 
-void liberar(int e, int k) {
-  
+void liberar(int e, int k) { // e es el índice del primer estacionamiento donde está, k es el número de estacionamientos requerido
+  // Se invoca cuando un automovilista se va
+  // Libera todos los estacionamientos reservados por un automovilista con reservar
 } 
