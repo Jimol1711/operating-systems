68de7cba49677185c521ed6625fd1645c13c4de1
Jimol1711 <juan.molina.l@ug.uchile.cl>
2024-04-04 12:41:04 -0300

    T2 finished

--- Tareas/T2/reservar.c
+++ Tareas/T2/reservar.c
@@ -9,8 +9,9 @@
 
 pthread_mutex_t mutex;
 pthread_cond_t cond;
-int disponibilidad[10] = {0}; // 0 significa disponible, 1 significa ocupado
+int disponibilidad[10] = {0};
 int colaEspera = 0;
+int turno = 0;
 
 void initReservar() {
     pthread_mutex_init(&mutex, NULL);
@@ -22,41 +23,45 @@ void cleanReservar() {
     pthread_cond_destroy(&cond);
 }
 
-int reservar(int k) { // k es el número de estacionamientos requerido
-    // Si hay k estacionamientos contiguos los reserva y retorna id del primer estacionamiento en la serie otorgada
-    // En caso contrario, espera hasta que hayan k estacionamientos contiguos disponibles.
+int reservar(int k) {
     pthread_mutex_lock(&mutex);
+    int miTurno = colaEspera++;
+    while (miTurno != turno) {
+        pthread_cond_wait(&cond, &mutex);
+    }
+
     int i, j;
     while (1) {
         // Buscar k estacionamientos contiguos disponibles
-        for (i = 0; i <= 10 - k; ++i) {
-            for (j = i; j < i + k; ++j) {
-                if (disponibilidad[j] == 1) break; // Estacionamiento ocupado
+        for (i = 0; i <= 10 - k; i++) {
+            for (j = i; j < i + k; j++) {
+                if (disponibilidad[j] == 1) 
+                    break; // Estacionamiento ocupado
             }
-            if (j == i + k) break; // Se encontraron k estacionamientos contiguos disponibles
+            if (j == i + k) 
+                break; // Se encontraron k estacionamientos contiguos disponibles
         }
-        if (i <= 10 - k) break; // Se encontraron k estacionamientos contiguos disponibles
+        if (i <= 10 - k) 
+            break; // Se encontraron k estacionamientos contiguos disponibles
         // Si no hay suficientes estacionamientos disponibles, esperar
-        colaEspera++;
         pthread_cond_wait(&cond, &mutex);
-        colaEspera--;
     }
 
     // Reservar los estacionamientos encontrados
-    for (j = i; j < i + k; ++j) {
+    for (j = i; j < i + k; j++) {
         disponibilidad[j] = 1;
     }
 
+    turno++;
+    pthread_cond_broadcast(&cond);
     pthread_mutex_unlock(&mutex);
     return i;
 }
 
-void liberar(int e, int k) { // e es el índice del primer estacionamiento donde está, k es el número de estacionamientos requerido
-    // Se invoca cuando un automovilista se va
-    // Libera todos los estacionamientos reservados por un automovilista con reservar
+void liberar(int e, int k) {
     pthread_mutex_lock(&mutex);
     // Liberar los estacionamientos reservados
-    for (int i = e; i < e + k; ++i) {
+    for (int i = e; i < e + k; i++) {
         disponibilidad[i] = 0;
     }
     // Notificar a otros vehículos en espera
