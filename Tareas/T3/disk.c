1bd0406f919d0f9fc9a1013065ab47de33fddec3
Jimol1711 <juan.molina.l@ug.uchile.cl>
2024-04-20 21:52:43 -0400

    Slow and steady

--- Tareas/T3/disk.c
+++ Tareas/T3/disk.c
@@ -13,9 +13,10 @@
  *****************************************************/
 // mutex, cola normal, cola de prioridad
 pthread_mutex_t m;
-Queue *q;
-PriQueue *priQ;
-int disk_busy = 0;
+PriQueue *priQ1;
+PriQueue *priQ2;
+int disk_busy;
+int current_track;
 
 // Estructura para la request
 typedef struct {
@@ -26,32 +27,30 @@ typedef struct {
 
 void iniDisk(void) {
     pthread_mutex_init(&m, NULL);
-    q = makeQueue();
-    priQ = makePriQueue();
+    priQ1 = makePriQueue();
+    priQ2 = makePriQueue();
+    disk_busy = 0;
 }
 
 void cleanDisk(void) {
     pthread_mutex_destroy(&m);
-    destroyQueue(q);
-    destroyPriQueue(priQ);
+    destroyPriQueue(priQ1);
+    destroyPriQueue(priQ2);
 }
 
 void requestDisk(int track) {
     pthread_mutex_lock(&m);
 
-    if (disk_busy || !emptyPriQueue(priQ)) {
-        Request *req = malloc(sizeof(Request));
-        req->ready = 0;
-        req->my_track = track;
-        pthread_cond_init(&(req->c), NULL);
-        priPut(priQ, req, req->my_track);
+    if (disk_busy || !emptyPriQueue(priQ1)) {
+        Request req = {0, track, PTHREAD_COND_INITIALIZER};
+        priPut(priQ1, &req, req.my_track);
 
-        while (!req->ready)
-            pthread_cond_wait(&(req->c), &m);
+        while (!req.ready)
+            pthread_cond_wait(&(req.c), &m);
 
-        free(req);
     } else {
         disk_busy = 1;
+        current_track = track;
     }
 
     pthread_mutex_unlock(&m);
@@ -60,10 +59,18 @@ void requestDisk(int track) {
 void releaseDisk() {
     pthread_mutex_lock(&m);
 
-    if (!emptyPriQueue(priQ)) {
-        Request *req = (Request *)priGet(priQ);
-        req->ready = 1;
-        pthread_cond_signal(&(req->c));
+    if (!emptyPriQueue(priQ1)) {
+        Request *next = priPeek(priQ1);
+        if (next->my_track >= current_track) {
+            Request *req = priGet(priQ1);
+            req->ready = 1;
+            current_track = req->my_track;
+            pthread_cond_signal(&(req->c));
+        } else {
+            Request *req1 = priGet(priQ1);
+            priPut(priQ2, req1, req1->my_track);
+
+        }
     } else {
         disk_busy = 0;
     }
