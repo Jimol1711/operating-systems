bc9210d60cda194a8ba9d7bb4f28b788f349b12d
Jimol1711 <juan.molina.l@ug.uchile.cl>
2024-04-26 19:13:05 -0400

    T3 Finished

--- Tareas/T3/disk.c
+++ Tareas/T3/disk.c
@@ -7,16 +7,13 @@
 #include "disk.h"
 #include "pss.h"
 
-/*****************************************************
- * Agregue aca los tipos, variables globales u otras
- * funciones que necesite
- *****************************************************/
 // mutex, cola normal, cola de prioridad
 pthread_mutex_t m;
 PriQueue *priQ;
+PriQueue *priQ2;
 Queue *q;
-int disk_busy = 0;
-int current_track = 0;
+int disk_busy;
+int current_track;
 
 // Estructura para la request
 typedef struct {
@@ -28,30 +25,34 @@ typedef struct {
 void iniDisk(void) {
     pthread_mutex_init(&m, NULL);
     priQ = makePriQueue();
+    priQ2 = makePriQueue();
+    q = makeQueue();
+    disk_busy = 0;
+    current_track = 0;
 }
 
 void cleanDisk(void) {
     pthread_mutex_destroy(&m);
     destroyPriQueue(priQ);
+    destroyPriQueue(priQ2);
+    destroyQueue(q);
 }
 
 void requestDisk(int track) {
     pthread_mutex_lock(&m);
 
-    if (disk_busy || !emptyPriQueue(priQ)) {
-        Request *req = malloc(sizeof(Request));
-        req->ready = 0;
-        req->my_track = track;
-        pthread_cond_init(&(req->c), NULL);
-        priPut(priQ, req, req->my_track);
-
-        while (!req->ready)
-            pthread_cond_wait(&(req->c), &m);
-
-        free(req);
-    } else {
+    if(!disk_busy) {
         disk_busy = 1;
         current_track = track;
+    } else {
+        Request req = {0, track, PTHREAD_COND_INITIALIZER};
+        if (track >= current_track) {
+            priPut(priQ, &req, track);
+        } else {
+            priPut(priQ2, &req, track);
+        }
+        while (!req.ready)
+            pthread_cond_wait(&(req.c), &m);
     }
 
     pthread_mutex_unlock(&m);
@@ -60,80 +61,26 @@ void requestDisk(int track) {
 void releaseDisk() {
     pthread_mutex_lock(&m);
 
-    #if 1
     if (!emptyPriQueue(priQ)) {
-
-        do {
-            Request *req = priGet(priQ);
-            if (req->my_track >= current_track) {
-                req->ready = 1;
-                current_track = req->my_track;
-
-            while(!emptyQueue(q)) {
-                Request *putBack = get(q);
-                priPut(priQ, putBack, putBack->my_track);
-            }
-
-                pthread_cond_signal(&(req->c));
-            } else {
-                put(q, req);
-            }
-            free(req);
-        } while(!emptyPriQueue(priQ));
-
-    } else {
-        disk_busy = 0;
-    }
-    #endif
-
-    #if 0
-    if (!emptyPriQueue(priQ)) {
-        Request *next;
-        do {
-            next = priPeek(priQ);
-            if (next->my_track < current_track) {
-                Request *lower = priGet(priQ);
-                put(q,lower);
-            } else {
-                while(!emptyQueue(q)) {
-                    Request *putBack = get(q);
-                    priPut(priQ,putBack,putBack->my_track);
-                }
-            }
-        } while(next->my_track < current_track || !emptyQueue(q));
-    } else {
-        disk_busy = 0;
-    }
-    #endif
-
-    #if 0
-
-    if (!emptyPriQueue(priQ)) {
-        Request *req = (Request *)priGet(priQ);
+        current_track = priBest(priQ);
+        Request *req = priGet(priQ);
         req->ready = 1;
         pthread_cond_signal(&(req->c));
     } else {
-        // Liberar el disco y avanzar al siguiente track según la estrategia C-SCAN
-        disk_busy = 0;
-        if (!emptyQueue(q)) {
-            int next_track = current_track + 1;
-
-            while (!emptyQueue(q)) {
-                Request *req = (Request *)get(q);
-                if (req->my_track == next_track) {
-                    disk_busy = 1;
-                    current_track = next_track;
-                    pthread_cond_signal(&(req->c));
-                    free(req);
-                    break;
-                } else {
-                    put(q, req);
-                }
-            }
+        while(!emptyPriQueue(priQ2)) {
+            int my_track = priBest(priQ2);
+            Request *lower = priGet(priQ2);
+            priPut(priQ, lower, my_track);
+        }
+        if (emptyPriQueue(priQ)) {
+            disk_busy = 0;
+        } else {
+            current_track = priBest(priQ);
+            Request *req = priGet(priQ);
+            req->ready = 1;
+            pthread_cond_signal(&(req->c));
         }
     }
-
-    #endif
 
     pthread_mutex_unlock(&m);
 }
