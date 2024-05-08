81200a2d861e0f2ef2519d421d2bf7455b4edb26
Jimol1711 <juan.molina.l@ug.uchile.cl>
2024-05-08 17:54:06 -0400

    starting T4

--- Tareas/T4/rwlock.c
+++ Tareas/T4/rwlock.c
@@ -5,31 +5,32 @@
 #include "rwlock.h"
 
 struct rwlock {
-  ... implementa aca la estructura del tipo nRWLock ...
+  int nth_ready;
+  
 };
 
 nRWLock *nMakeRWLock() {
-  ...
+  
 }
 
 void nDestroyRWLock(nRWLock *rwl) {
-  ...
+  
 }
 
 int nEnterRead(nRWLock *rwl, int timeout) {
-  ...
+
   return 1;
 }
 
 int nEnterWrite(nRWLock *rwl, int timeout) {
-  ...
+
   return 1;
 }
 
 void nExitRead(nRWLock *rwl) {
-  ...
+  
 }
 
 void nExitWrite(nRWLock *rwl) {
-  ...
+  
 }
